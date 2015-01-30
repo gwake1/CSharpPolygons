040f05934290f432c28f4ad6b0bdd4fe0789d0c9
gwake1 <gwake1@gmail.com>
2015-01-30 17:31:19 -0600

    unit test for shapes class

--- UnitTestSharpShapes/UnitTestShapes.cs
+++ UnitTestSharpShapes/UnitTestShapes.cs
@@ -3,14 +3,62 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing;
 using SharpShapes;
 
-namespace UnitTestSharpShapes
+namespace TestSharpShapes
 {
     [TestClass]
-    public class UnitTestShapes
+    public class UnitTestShape
     {
-        [TestMethod]
-        public void TestMethod1()
+        public class MyShape : Shape
         {
+            public override int SidesCount
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public override void Scale(int percent)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override decimal Perimeter()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override decimal Area()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        public void TestSettingBorderColor()
+        {
+            Shape shape = new MyShape();
+            shape.BorderColor = Color.AliceBlue;
+            Assert.AreEqual(Color.AliceBlue, shape.BorderColor);
+        }
+
+        [TestMethod]
+        public void TestDefaultBorderColor()
+        {
+            Shape shape = new MyShape();
+            Assert.AreEqual(Color.Tomato, shape.BorderColor);
+        }
+
+        [TestMethod]
+        public void TestSettingFillColor()
+        {
+            Shape shape = new MyShape();
+            shape.FillColor = Color.AliceBlue;
+            Assert.AreEqual(Color.AliceBlue, shape.FillColor);
+        }
+
+        [TestMethod]
+        public void TestDefaultFillColor()
+        {
+            Shape shape = new MyShape();
+            Assert.AreEqual(Color.Bisque, shape.FillColor);
         }
     }
 }
