b5bea972c955b4c5908b6d6e02b6a894f2410805
gwake1 <gwake1@gmail.com>
2015-01-30 17:30:19 -0600

    test project created

---
+++ UnitTestSharpShapes/UnitTestShapes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using SharpShapes;
+
+namespace UnitTestSharpShapes
+{
+    [TestClass]
+    public class UnitTestShapes
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
